5779459ee4d29d169868031fa2f1699afab2bed5
Felipe Gutierrez <caracoloro@gmail.com>
2019-05-28 18:11:15 -0400

    Firebase

--- src/app/services/info-pages.service.ts
+++ src/app/services/info-pages.service.ts
@@ -8,15 +8,30 @@ export class InfoPagesService {
 
   info: InfoPages = {};
   loaded = false;
+  equipo: any [] = [];
 
   constructor( private http: HttpClient ) {
 
+    this.loadInfo();
+
+    this.loadTeam();
+  }
+
+  private loadInfo(){
     // Leer el archivo data-pages.json
     this.http.get('assets/data/data-pages.json')
     .subscribe( ( resp: InfoPages) => {
       this.loaded = true;
       this.info = resp;
+    });
+  }
+
+  private loadTeam(){
+    this.http.get('https://templatetohtml.firebaseio.com/equipo.json')
+    .subscribe( ( resp: any) => {
+      this.loaded = true;
+      this.equipo = resp;
       console.log(resp);
-    })
-   }
+    });
+  }
 }
