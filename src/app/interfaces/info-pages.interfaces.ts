5c2cec054dcc081b338e2a8c5a5dd765c450bdd1
Felipe Gutierrez <caracoloro@gmail.com>
2019-05-28 16:17:26 -0400

    Interfaces

---
+++ src/app/interfaces/info-pages.interfaces.ts
@@ -0,0 +1,11 @@
+interface InfoPages {
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    linkedin?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    equipo_trabajo?: any[];
+}
